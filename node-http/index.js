a83494a8f5d4b2e259b53428ba99d8a445019f55
jesusquev3311 <jesusquev3311@gmail.com>
2018-12-31 23:14:29 -0500

    http node expmples done

--- node-http/index.js
+++ node-http/index.js
@@ -1,13 +1,44 @@
 const http = require('http');
-
+const fs   = require('fs');
+const path = require('path');
 const port =  3000;
 
 const server = http.createServer((req,res) =>{
-    console.log(req.headers);
+    console.log(`Request for ${req.url} by method ${req.method}`);
 
-    res.statusCode = 200;
-    res.setHeader('content-type', 'text/html');
-    res.end('<html><body><h1>Hello World</h1></body></html>')
+    if(req.method === 'GET'){
+        let fileURL;
+        req.url === '/'? fileURL = 'index.html' : fileURL = req.url;
+        
+        let filePath = path.resolve('./public/' + fileURL);
+        console.log(filePath)
+        const fileExt = path.extname(filePath);
+
+        if(fileExt == '.html'){
+            
+            fs.exists(filePath, (exist) =>{
+                if(!exist){
+                    res.statusCode = 404;
+                    res.setHeader('content-tytpe', 'text/html');
+                    res.end(`<html><h1>Error 404: ${fileURL} not found !!!</h1></html>`);
+                    return
+                }
+                res.statusCode = 200;
+                res.setHeader('content-tytpe', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+            })
+        } else {
+            res.statusCode = 404;
+            res.setHeader('content-tytpe', 'text/html');
+            res.end(`<html><h1>Error 404: ${fileURL} is not a HTML file !!!</h1></html>`);
+            return
+        }
+    } else {
+        res.statusCode = 404;
+        res.setHeader('content-tytpe', 'text/html');
+        res.end(`<html><h1>Error 404: ${req.method} is not GET method !!!</h1></html>`);
+        return
+    }
 });
 
 server.listen(port, (req, res) => {
