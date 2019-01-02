04ed3ef83c9ae843ca9da5443c3e80a60c4b3519
jesusquev3311 <jesusquev3311@gmail.com>
2019-01-01 23:25:21 -0500

    assignment 1 started

--- node-express/index.js
+++ node-express/index.js
@@ -11,30 +11,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use('/dishes', dishRouter);
 
-/* Assignment 1
-//Single Items 
-//GET request
-app.get('/dishes/dishId', (req,res,next)=>{
-    res.end('Will send details of dish: ' + req.params.dishId );
-});
 
-//POST Method
-app.post('/dishes/dishId', (req, res, next) =>{
-    res.statusCode(403);
-    res.end('POST is not supportted on Dish: ' + req.params.dishId);
-});
-
-//PUT Method
-app.put('/dishes/dishId', (req, res, next) =>{
-    res.write('Will update the Dish: ' + req.params.dishId)
-    res.end('will update the Dish: ' + req.body.name +
-    'Will update the descripton: ' + req.body.description);
-});
-
-//DELETE Method
-app.delete('/dishes/dishId', (req, res, next) =>{
-    res.end('This will Delete the dish: ' + req.params.dishId);
-});*/
 
 app.use((req,res,next) =>{
     res.statusCode = 200;
--- node-express/routes/dishRouter.js
+++ node-express/routes/dishRouter.js
@@ -35,4 +35,32 @@ dishRouter.route('/')
     res.end('This will Delete all the dishes');
 });
 
+//single post
+dishRouter.route('/:dishId')
+// Assignment 1
+
+//Single Items 
+//GET request
+.get((req,res,next)=>{
+    res.end('Will send details of dish: ' + req.params.dishId );
+})
+
+//POST Method
+.post((req, res, next) =>{
+    res.statusCode(403);
+    res.end('POST is not supportted on Dish: ' + req.params.dishId);
+})
+
+//PUT Method
+.put( (req, res, next) =>{
+    res.write('Will update the Dish: ' + req.params.dishId)
+    res.end('will update the Dish: ' + req.body.name +
+    'Will update the descripton: ' + req.body.description);
+})
+
+//DELETE Method
+.delete((req, res, next) =>{
+    res.end('This will Delete the dish: ' + req.params.dishId);
+});
+
 module.exports = dishRouter;
