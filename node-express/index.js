86e53e448e2fa804ef39bfb1c15c197f93adcded
jesusquev3311 <jesusquev3311@gmail.com>
2019-01-01 23:48:42 -0500

    promo, leaders routes added

--- node-express/index.js
+++ node-express/index.js
@@ -5,11 +5,15 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 
 
