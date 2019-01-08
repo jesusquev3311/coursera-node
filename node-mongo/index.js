48939bd44c77f0e47050142ab4bce74a6d968f9a
jesusquev3311 <jesusquev3311@gmail.com>
2019-01-07 20:25:57 -0500

    m

---
+++ node-mongo/index.js
@@ -0,0 +1,37 @@
+const mongoClient = require('mongodb');
+const assert      = require('assert');
+
+const url         = "mongodb://localhost:27017/";
+const dbname      = "conFusion";
+
+mongoClient.connect(url, (err, client) =>{
+    assert.equal(err, null);
+
+    console.log('connectedmcurrently to server');
+
+    const db = client.db(dbname);
+    const collection = db.collection('dishes');
+
+    collection.insertOne({
+        "name": "Uthapizza",
+        "description": "Test"
+    }, (err, result) =>{
+        assert.equal(err, null);
+
+        console.log('After insert: \n');
+        console.log(result.ops);
+
+        collection.find({}).toArray((err, docs) =>{
+            assert.equal(err, null);
+
+            console.log('Found:\n');
+            console.log(docs);
+
+            db.dropCollection('dishes', (err, result)=>{
+                assert.equal(err, null);
+
+                client.close();
+            });
+        });
+    });
+});
