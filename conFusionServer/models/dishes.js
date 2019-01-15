2f1e279dce801958b562aeb96400c77a3c74d8ee
jesusquev3311 <jesusquev3311@gmail.com>
2019-01-14 23:01:17 -0500

    confusion server models added

---
+++ conFusionServer/models/dishes.js
@@ -0,0 +1,39 @@
+//import the moongose pkg
+const moongose = require('mongoose');
+
+const Schema = moongose.Schema;
+
+const commentSchema = new Schema({
+    rating:{
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment:{
+        type: String,
+        required: true
+    }
+},
+{
+    timestamps: true
+});
+
+const dishSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    comments: [commentSchema]
+}, {
+    timestamps: true
+});
+
+let Dishes = moongose.model('dish', dishSchema);
+
+module.exports = Dishes;
