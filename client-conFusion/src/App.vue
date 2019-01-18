72547f988a1be3788ff42b6189aaef02f80a6363
jesus quevedo <jesus@gokapital.com>
2019-01-18 16:09:20 -0500

    bootstrap and jquery added to the front

--- client-conFusion/src/App.vue
+++ client-conFusion/src/App.vue
@@ -12,6 +12,7 @@ export default {
 </script>
 
 <style>
+@import url('./../node_modules/bootstrap/dist/css/bootstrap.min.css');
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,5 +20,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
 }
 </style>
--- client-conFusion/src/components/Dishes.vue
+++ client-conFusion/src/components/Dishes.vue
@@ -1,27 +1,33 @@
 <template>
-    <div>
-        <h1>{{this.title}}</h1>
-
-        <div v-for="dish in dishes">
+  <section id="dishes">
+    <div class="container">
+      <h1>{{this.title}}</h1>
+      <div class="row">
+        <template v-for="dish in dishes">
+          <div class="col-sm-4">
             <div class="dish-item">
                 <span class="img-wrapper" > <img :src= "dish.image" alt=""></span>
                 <span><b>{{ dish.name }}</b></span><br />
                 <span>{{ dish.description }}</span>
             </div>
-        </div>
+          </div>
+        </template>
+      </div>
     </div>
+
+  </section>
 </template>
 
 <script>
 import DishesService from '@/services/DishesService'
 
 export default {
-    name: 'Dishes',
-     data () {
-    return {
-      dishes: [],
-      title: 'Dishes'
-    }
+  name: 'Dishes',
+    data () {
+      return {
+        dishes: [],
+        title: 'Dishes'
+      }
   },
   mounted () {
     this.getDishes()
@@ -36,14 +42,15 @@ export default {
 </script>
 
 <style lang="scss">
-    .img-wrapper{
-        img{
-            height: 240px;
-            width: 240px;
-            object-fit: contain;
-            display: inline-block;
-        }
+  .img-wrapper{
+    display: block;
+    img{
+        height: 240px;
+        width: 240px;
+        object-fit: contain;
+        display: inline-block;
     }
+  }
 </style>
 
 
