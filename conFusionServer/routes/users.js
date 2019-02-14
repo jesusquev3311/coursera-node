e8a686fbc739f17731bb332cfd7d399c7bf62ec8
jesus quevedo <jesus@gokapital.com>
2019-02-14 18:21:48 -0500

    signup route

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
-let User = require('./models/user');
+let User = require('../models/user');
 
 router.use(bodyParser.json());
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 });
+
+router.post('/signup', (req, res, next) => {
+    User.findOne({username: req.body.username})
+        .then((user) => {
+            if (user != null) {
+                var err = new Error(`User ${req.body.username} Already Exist`);
+                err.status(403);
+                next(err);
+            } else {
+                return User.create({
+                    username: req.body.username,
+                    password: req.body.password
+                });
+            }
+        })
+        .then((user) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'aplication/json');
+            res.json({
+                status: 'registration successful!',
+                user: user,
+            }, (err) => next(err))
+        })
+        .catch((err) => next(err));
+})
 
 module.exports = router;
